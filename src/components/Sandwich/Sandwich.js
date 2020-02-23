bd10c75a33bd714a1808f47f22109b5c1925deec
randyshao <shao.randy@gmail.com>
2020-02-23 01:09:06 -0500

    State changes for adding/removing ingredients

--- src/components/Sandwich/Sandwich.js
+++ src/components/Sandwich/Sandwich.js
@@ -3,14 +3,20 @@ import classes from './Sandwich.css';
 import Ingredient from './Ingredient/Ingredient';
 
 const sandwich = (props) => { 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <Ingredient key={igKey + i} type={igKey} />
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Build your sandwich!</p>;
+    }
     return (
-        <div className={classes.sandwich}>
+        <div className={classes.Sandwich}>
             <Ingredient type="bread-top" />
             {transformedIngredients}
             <Ingredient type="bread-bottom" />
--- src/containers/SandwichMaker/SandwichMaker.js
+++ src/containers/SandwichMaker/SandwichMaker.js
@@ -1,23 +1,71 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Sandwich from '../../components/Sandwich/Sandwich';
+import BuildControls from '../../components/Sandwich/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 0.4,
+    cheese: 0.5,
+    meat: 1.3,
+    bacon: 0.7
+}
 
 class SandwichMaker extends Component {
     state = {
         ingredients: {
-            lettuce: 1,
-            onion: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
+            lettuce: 0,
+            onion: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
         }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
     }
 
     render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
+        // {lettuce: true, meat: false, ...}
         return (
             <Aux>
                 <Sandwich ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler} 
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo} />
             </Aux>
         );
     }
