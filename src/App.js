2a7bcf5214a89c12dffda69581ad9c06ce13fefe
randyshao <shao.randy@gmail.com>
2020-02-27 19:08:46 -0500

    Checkout container and routing setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import SandwichMaker from './containers/SandwichMaker/SandwichMaker';
+import Checkout from './containers/Checkout/Checkout';
+
 
 class App extends Component {
+
   render() {
     return (
       <div>
         <Layout>
-          <SandwichMaker/>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
--- src/containers/SandwichMaker/SandwichMaker.js
+++ src/containers/SandwichMaker/SandwichMaker.js
@@ -18,17 +18,22 @@ const INGREDIENT_PRICES = {
 
 class SandwichMaker extends Component {
     state = {
-        ingredients: {
-            lettuce: 0,
-            onion: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        },
+        ingredients: null,
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
+    }
+
+    componentDidMount () {
+        axios.get('https://sandwich-maker-9b098.firebaseio.com/ingredients.json')
+            .then(response => {
+                this.setState({ingredients: response.data});
+            })
+            .catch(error => {
+                this.setState({error: true})
+            });
     }
 
     updatePurchaseState (ingredients) {
@@ -114,29 +119,38 @@ class SandwichMaker extends Component {
         for (let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
-        let orderSummary = <OrderSummary 
+        let orderSummary = null;
+        let sandwich = this.state.error ? <p>Ingredients can't be loaded</p> : <Spinner />
+
+        if (this.state.ingredients) {
+            sandwich = (
+                <Aux>
+                    <Sandwich ingredients={this.state.ingredients}/>
+                    <BuildControls
+                        ingredientAdded={this.addIngredientHandler} 
+                        ingredientRemoved={this.removeIngredientHandler}
+                        disabled={disabledInfo}
+                        purchasable={this.state.purchasable}
+                        ordered={this.purchaseHandler}
+                        price={this.state.totalPrice} />
+                </Aux>
+            );
+            orderSummary = <OrderSummary 
             ingredients={this.state.ingredients}
             price={this.state.totalPrice.toFixed(2)}
             purchaseCancelled={this.purchaseCancelHandler}
             purchaseContinued={this.purchaseContinueHandler} />
+        }
         if (this.state.loading) {
             orderSummary = <Spinner />;
-        }
-
+        };
         // {lettuce: true, meat: false, ...}
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     {orderSummary}
                 </Modal>
-                <Sandwich ingredients={this.state.ingredients}/>
-                <BuildControls
-                    ingredientAdded={this.addIngredientHandler} 
-                    ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}
-                    purchasable={this.state.purchasable}
-                    ordered={this.purchaseHandler}
-                    price={this.state.totalPrice} />
+                {sandwich}
             </Aux>
         );
     }
